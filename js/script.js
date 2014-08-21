a67c249532b12581e3a8a90e41a7df1c2593e48b
Denis <denis@gridchin.com>
2014-08-22 01:16:39 +0200

    Menu

--- js/script.js
+++ js/script.js
@@ -6,19 +6,25 @@ $(document).ready(function() {
 	// },1000);
 	
 	// scroll effect
-	$(".main").onepage_scroll({
+	$('.main').onepage_scroll({
 		sectionContainer: "section",
 		responsiveFallback: 600,
-		pagination: true,
+		pagination: false,
 		loop: false,
 		updateUrl: true,
 		responsiveFallback: 600
 	});
 
+	// keep scrolling
 	$('.title a').click(function(){
 		$(".main").moveDown();
 		return false;
 	})
+
+	// menu
+	$('.modal a').click(function() {
+		$('#modal').modal('hide');
+	});
 	
 	// iframe fix on FF
 	var isFirefox = typeof InstallTrigger !== 'undefined'; 
